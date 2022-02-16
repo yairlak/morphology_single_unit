3e0aa9648b364c9ba5f391f5aba612a24bfa509e
Cognitive Neurophys Lab <cnl@cognitives-mbp.ad.medctr.ucla.edu>
2022-02-16 12:01:15 -0800

    EM: Added Morphology Blocks 5 and 6, which are a visual and audio block of real words instead of pseudowords, along with corresponding stimuli

---
+++ code/paradigm/morphology_block_6.m
@@ -0,0 +1,22 @@
+% Auditory block  
+clear all; close all; clc
+makeDiary = 1;
+pth = fileparts(which('morphology_block_6')); cd(pth);
+addpath('functions')
+block_type = 'auditory';
+stimulus_type = 'realwords';
+repetitions = 8;
+% For compatibility with the visual blocks,
+% the next three lines should be left unchanged
+fonts = {'LiberationMono-Regular.ttf'};
+letter_cases = {'lower'};
+positions = {'center'};
+%
+n_blocks = 3;
+stimulus_ontime = 0.25;
+if makeDiary
+diary('WhyIsThisFreezing')
+disp('about to call morphology single unt')
+end
+morphology_single_unit
+
